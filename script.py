f65d940fc82f287228bb18e95b45a679981691b5
infni1111 <infni1111@gmail.com>
2025-12-13 19:17:07 +0000

    script.py

--- script.py
+++ script.py
@@ -1,1 +1,14 @@
-print("infni on gh")
+from gtts import gTTS
+
+# Texte à convertir en audio
+
+# Créer un objet gTTS en français
+tts = gTTS(text=text, lang='fr')
+
+ospf=[
+    "zone ospf"
+]
+# Sauvegarder le fichier audio
+tts.save("gtts.mp3")
+
+print("Audio généré : pression_gtts.mp3")
